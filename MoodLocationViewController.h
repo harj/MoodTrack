504005569acaf1b040283a3234423b215c815d00
harj <harjeet.taggar@gmail.com>
2012-05-07 21:10:27 -0700

    displaying map view when clicking on mood log cell

---
+++ MoodLocationViewController.h
@@ -0,0 +1,14 @@
+//
+//  MoodLocationViewController.h
+//  MoodTrack
+//
+//  Created by Harjeet Taggar on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MoodLocationViewController : UIViewController
+- (IBAction)closeMoodLocation:(id)sender;
+
+@end
---
+++ MoodLocationViewController.m
@@ -0,0 +1,37 @@
+//
+//  MoodLocationViewController.m
+//  MoodTrack
+//
+//  Created by Harjeet Taggar on 5/7/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import "MoodLocationViewController.h"
+
+@interface MoodLocationViewController ()
+
+@end
+
+@implementation MoodLocationViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)viewDidUnload
+{
+    [super viewDidUnload];
+    // Release any retained subviews of the main view.
+}
+
+- (BOOL)shouldAutorotateToInterfaceOrientation:(UIInterfaceOrientation)interfaceOrientation
+{
+    return (interfaceOrientation == UIInterfaceOrientationPortrait);
+}
+
+- (IBAction)closeMoodLocation:(id)sender {
+    [self.presentingViewController dismissModalViewControllerAnimated:YES];
+}
+@end
