8f82a55ad767d8554ff5d958f2e0a0b24b5e26fd
harj <harjeet.taggar@gmail.com>
2012-06-06 16:37:53 -0700

    removed notifications rails app

--- notifications/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Notifications::Application.config.secret_token = '4f8f8b88027fc812e8e0e29d3ec4905e4c69e51854e665eea9619234061b1989029ef9187d13b90fee07b91dfd178d8c561ad987529717f5d43b7a5b9dcef68a'
